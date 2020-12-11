0ba0522cdc03e6e8fe135170e3ee01b537a230e0
Jeremie Langevin <jeremie.langevin@outlook.com>
2020-12-10 20:28:29 -0500

    Now have category and cover

--- includes/class-cw-cron-update-stock.php
+++ includes/class-cw-cron-update-stock.php
@@ -1,6 +1,7 @@
 <?php
 
 use CodesWholesale\Resource\Product;
+use CodesWholesale\Resource\ImageType;
 use CodesWholesale\Resource\FullProduct;
 use CodesWholesale\Resource\StockAndPriceChange;
 use CodesWholesaleFramework\Model\ProductModel;
@@ -52,6 +53,14 @@ if (!class_exists('CW_Update_Stock')) :
             $products_ids = array();
 
 			$cw_products = CW()->get_codes_wholesale_client()->getProducts();
+			
+			$cw_products_names_array = array();
+
+			$cw_product_id = array();
+			
+			foreach ($cw_products as $cw_product) {
+				$cw_products_names_array[] =  htmlspecialchars_decode($cw_product->getIdentifier());
+			}
 
 			$products = get_posts(array(
                 'post_type' => 'product',
@@ -66,37 +75,20 @@ if (!class_exists('CW_Update_Stock')) :
             ));
 			
 			foreach ($products as $product) {
-                $cw_product_id = get_post_meta($product->ID, CodesWholesaleConst::PRODUCT_CODESWHOLESALE_ID_PROP_NAME, true);
-                $products_ids[$cw_product_id] = $product;
+				$cw_product_id = get_post_meta($product->ID, CodesWholesaleConst::PRODUCT_CODESWHOLESALE_ID_PROP_NAME, true);
+				$products_ids[$cw_product_id] = $product;
             }
-						
-			$args = get_posts(array(
-				'post_type' => 'product',
-				'meta_query' => array(
-					array(
-						'key' => CodesWholesaleConst::PRODUCT_CODESWHOLESALE_ID_PROP_NAME,
-						'compare' => '!=',
-						'value' => ''
-					)
-				),
-				'numberposts' => -1
-			));
+			
+			$cw_products_not_set = array_diff($cw_products_names_array, $cw_product_id);
+			$cw_products_no_more = array_diff($cw_product_id, $cw_products_names_array);
 
-			foreach ($args as $post) { 
-				$produits[] = get_post_meta($post->ID, CodesWholesaleConst::PRODUCT_CODESWHOLESALE_ID_PROP_NAME, true);
-			}
-
+			sort($cw_products);
+			
 			foreach ($cw_products as $cw_product) {
-				$cw_produits[] = $cw_product->getProductId();
-			}
-
-			$produits_diff = array_diff($cw_produits, $produits);
-
-			foreach ($cw_products as $cw_product) {
-				if (in_array($cw_product->getProductId(), $produits_diff)) {
+				echo $cw_product->getName() . "\n";
+				if (in_array(htmlspecialchars_decode($cw_product->getIdentifier()), $cw_products_not_set)) {
 
 					echo "Adding Product... \n";
-					echo $cw_product->getName() . "\n";
 
 					if ( $cw_product->getReleaseDate() == '' ) {
 						$post = array(
@@ -139,47 +131,108 @@ if (!class_exists('CW_Update_Stock')) :
 					update_post_meta( $post_id, '_codeswholesale_product_spread_type', "0" );
 					update_post_meta( $post_id, '_codeswholesale_product_is_in_sale', "0" );
 
-				} else {
-					echo "Updateing Product... \n";
 				}
+
+				echo "Updateing Product... \n";
+
+				$post_product = $products_ids[$cw_product->getProductId()];
+
+				if ($post_product == null) {
+					$post_product->ID = $post_id;
+				}
+				
+				$price = $cw_product->getDefaultPrice();
 			
-                if (isset($products_ids[$cw_product->getProductId()])) {
-
-					echo $cw_product->getName() . "\n";	
-
-                    $post_product = $products_ids[$cw_product->getProductId()];
-                    
-                    $price = $cw_product->getDefaultPrice();
+				$priceSpread = $this->calculateSpread($this->getSpreadParams(), $price);
 				
-					$priceSpread = $this->calculateSpread($this->getSpreadParams(), $price);
-                    
-                    $product_spread_type =  get_post_meta($post_product->ID, "_codeswholesale_product_spread_type", true);
-					
-					$product_is_in_sale =  get_post_meta($post_product->ID, "_codeswholesale_product_is_in_sale", true);
-																			
-					if ($cw_product->getStockQuantity() > 0) {
-						update_post_meta($post_product->ID, "_stock_status", "instock");
-						update_post_meta($post_product->ID, '_manage_stock', "yes" );
-						update_post_meta($post_product->ID, "_stock", esc_attr($cw_product->getStockQuantity())); 
-					}
-						
-					if ($product_spread_type == 0) {
-						if ($cw_product->getDefaultPrice() > 0) {
-							if ($product_is_in_sale == 0) {
-								update_post_meta($post_product->ID, "_regular_price", esc_attr(round($priceSpread , 2)));
-								update_post_meta($post_product->ID, "_price", esc_attr(round($priceSpread , 2)));
-							} else {
-								update_post_meta($post_product->ID, "_sale_price", esc_attr(round($priceSpread , 2)));
-								update_post_meta($post_product->ID, "_price", esc_attr(round($priceSpread , 2)));
-							}
-							update_post_meta($post_product->ID, "_virtual", "yes");
-						} else {
-							update_post_meta($post_product->ID, "_regular_price", esc_attr(0));
-							update_post_meta($post_product->ID, "_price", esc_attr(0));
-						}
-					}
+				$product_spread_type =  get_post_meta($post_product->ID, "_codeswholesale_product_spread_type", true);
 				
+				$product_is_in_sale =  get_post_meta($post_product->ID, "_codeswholesale_product_is_in_sale", true);
+																		
+				if ($cw_product->getStockQuantity() > 0) {
+					update_post_meta($post_product->ID, "_stock_status", "instock");
+					update_post_meta($post_product->ID, '_manage_stock', "yes" );
+					update_post_meta($post_product->ID, "_stock", esc_attr($cw_product->getStockQuantity())); 
 				}
+					
+				if ($product_spread_type == 0) {
+					if ($cw_product->getDefaultPrice() > 0) {
+						if ($product_is_in_sale == 0) {
+							update_post_meta($post_product->ID, "_regular_price", esc_attr(round($priceSpread , 2)));
+							update_post_meta($post_product->ID, "_price", esc_attr(round($priceSpread , 2)));
+						} else {
+							update_post_meta($post_product->ID, "_sale_price", esc_attr(round($priceSpread , 2)));
+							update_post_meta($post_product->ID, "_price", esc_attr(round($priceSpread , 2)));
+						}
+						update_post_meta($post_product->ID, "_virtual", "yes");
+					} else {
+						update_post_meta($post_product->ID, "_regular_price", esc_attr(0));
+						update_post_meta($post_product->ID, "_price", esc_attr(0));
+					}
+				}
+
+				$category_id = array();
+
+				$Platform = get_term_by('name', 'Platform', 'product_cat');
+				$Region = get_term_by('name', 'Region', 'product_cat');
+				$Language = get_term_by('name', 'Language', 'product_cat');
+
+				$getRegions = $cw_product->getRegions();
+				$getLanguages = $cw_product->getLanguages();
+
+				$Platforms = get_term_by('name', str_replace(' ', '-', $cw_product->getPlatform()), 'product_cat');
+
+				
+				if ( empty( $Platform ) ) {
+					wp_insert_term( 'Platform', 'product_cat', array( 'slug' => 'platform'));
+				}
+
+				if ( empty( $Platforms ) ) {
+					wp_insert_term( $cw_product->getPlatform(), 'product_cat', array( 'slug' => $Platforms, 'parent'=> term_exists( 'Platform', 'product_cat' )['term_id'] ));
+				}					
+				$category_id[0] = $Platforms->term_id;
+				
+				if ( empty( $Region ) ) {
+					wp_insert_term( 'Region', 'product_cat', array( 'slug' => 'region'));
+				}
+
+				foreach ($getRegions as $getRegion) {
+					$Regions  = get_term_by('slug', str_replace(' ', '-', $getRegion), 'product_cat');
+					if ( empty( $Regions ) ) {
+						wp_insert_term(  $getRegion, 'product_cat', array( 'slug' => $getRegion, 'parent'=> term_exists( 'Region', 'product_cat' )['term_id'] ));
+						$Regions  = get_term_by('slug', str_replace(' ', '-', $getRegion), 'product_cat');
+					}
+					$category_id[1] = $Regions->term_id;
+					echo $getRegion;
+				}
+				
+				if ( empty( $Language ) ) {
+					wp_insert_term( 'Language', 'product_cat', array( 'slug' => 'language'));
+				}
+
+				foreach ($getLanguages as $getLanguage) {
+					$Languages = get_term_by('slug', str_replace(' ', '-', $getLanguage), 'product_cat');
+					if ( empty( $Languages ) ) {
+						wp_insert_term(  $getLanguage, 'product_cat', array( 'slug' => $getLanguage, 'parent'=> term_exists( 'Language', 'product_cat' )['term_id'] ));
+						$Languages = get_term_by('slug', str_replace(' ', '-', $getLanguage), 'product_cat');
+					}
+					$category_id[2] = $Languages->term_id;
+					echo  $getLanguage;
+				}
+
+				$term_lists = wp_get_post_terms( $post_product->ID, 'product_cat', array( 'fields' => 'ids' ) );
+				
+				//echo  $cw_product->getName() . "\n";					
+				$category_ids_ = array_merge($category_id, $term_lists);
+				
+				wp_set_post_terms($post_product->ID, array_unique($category_ids_), "product_cat");
+				update_post_meta($post_product->ID, "product_cat", array_unique($category_ids_));
+				
+				update_post_meta($post_product->ID, "post_title", htmlspecialchars(esc_attr($cw_product->getName())));
+
+				// Returns Array of Term Names for "my_taxonomy".
+				$term_list = wp_get_post_terms( $post_product->ID, 'product_cat', array( 'fields' => 'names' ) );
+				print_r( $term_list );
 
 				$cw_format_date = strtotime(str_replace( array('T',':00.000Z'), ' ', $cw_product->getReleaseDate() )); 
 				update_post_meta( $post_product->ID, "_wc_pre_orders_availability_datetime", $cw_format_date );
@@ -223,14 +276,56 @@ if (!class_exists('CW_Update_Stock')) :
 					}
 				}
 
-			}
+				if ( !has_post_thumbnail($post_product->ID) ) {
+					$cw_img_url = $cw_product->getImageUrl(ImageType::MEDIUM);
+					//update_post_meta( $post_product->ID, "_cw_image_url", esc_attr($cw_img));
+					
+					if( !strpos($cw_img_url, "no-image") ) {													
+						// Add Featured Image to Post
+						$upload_dir = wp_upload_dir(); // Set upload folder
+						$image_data = file_get_contents($cw_img_url); // Get image data
+						$filename   = basename($cw_product->getProductId() . ".png"); // Create image file name
 
-			foreach ($cw_products as $cw_product) {
-				/* Create category */
-				$producModel = ProductModelFactory::resolveProduct($cw_product, CW()->get_options()[CodesWholesaleConst::PREFERRED_LANGUAGE_FOR_PRODUCT_OPTION_NAME]);
-				WP_Product_Updater::getInstance()->updateProduct->update($producModel, $post_product->ID);
+						// Check folder permission and define file location
+						if( wp_mkdir_p( $upload_dir['path'] ) ) {
+							$file = $upload_dir['path'] . '/' . $filename;
+						} else {
+							$file = $upload_dir['basedir'] . '/' . $filename;
+						}
 
-				echo $cw_product->getName() . "\n";	
+						// Create the image  file on the server
+						file_put_contents( $file, $image_data );
+
+						// Check image file type
+						$wp_filetype = wp_check_filetype( $filename, null );
+
+						// Set attachment data
+						$attachment = array(
+							'post_mime_type' => $wp_filetype['type'],
+							'post_title'     => sanitize_file_name( $cw_product->getName() ),
+							'post_content'   => '',
+							'post_author'   => '1',
+							'post_status'    => 'publish'
+						);
+
+						// Create the attachment
+						$attach_id = wp_insert_attachment( $attachment, $file, $post_product->ID );
+
+						// Include image.php
+						require_once(ABSPATH . 'wp-admin/includes/image.php');
+
+						// Define attachment metadata
+						$attach_data = wp_generate_attachment_metadata( $attach_id, $file );
+
+						// Assign metadata to attachment
+						wp_update_attachment_metadata( $attach_id, $attach_data );
+
+						// And finally assign featured image to post
+						set_post_thumbnail( $post_product->ID, $attach_id );
+						
+						echo $cw_img_url . "\n";
+					}
+				}	
 			}
 			
 			echo "Stock updated. \n";
